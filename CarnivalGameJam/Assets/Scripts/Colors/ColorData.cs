2372f51f892836f05893ec0c1389da45b6b66676
LapisRaider <angielimenqi7@gmail.com>
2021-08-03 18:52:57 +0800

    - set up the system, added singleton template and soundmanager - added a color Data stuff

---
+++ CarnivalGameJam/Assets/Scripts/Colors/ColorData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorData : SingletonBase<ColorData>
+{
+    [Header("Color Info")]
+    public ColorInfoData[] m_ColorInfoData;
+
+    private Dictionary<ColorVariants, ColorInfo> m_AvilableColorList = new Dictionary<ColorVariants, ColorInfo>(); // allow for easy access to color UI data
+
+    // Start is called before the first frame update
+    override public void Awake()
+    {
+        //transfer the color info to the dictionary
+        foreach (ColorInfoData colorInfoData in m_ColorInfoData)
+        {
+            colorInfoData.m_ColorInfo.m_ColorName = colorInfoData.m_Name;
+            m_AvilableColorList.Add(colorInfoData.m_ColorEnumType, colorInfoData.m_ColorInfo);
+        }
+    }
+
+    //get the color after mixing
+    public ColorVariants AddColor(ColorMixes currentColorMix)
+    {
+        //there should be a fuckin variant, if dont have just return the same current color
+        foreach (KeyValuePair<ColorVariants, ColorInfo> availableColor in m_AvilableColorList)
+        {
+            ColorMixes colorMixes = availableColor.Value.m_ColorToMix;
+
+            //check if have all the correct color type
+            if (currentColorMix.m_Red != colorMixes.m_Red)
+                continue;
+
+            if (currentColorMix.m_Blue != colorMixes.m_Blue)
+                continue;
+
+            if (currentColorMix.m_Yellow != colorMixes.m_Yellow)
+                continue;
+
+            return availableColor.Key;
+        }
+
+        return ColorVariants.COLORLESS;
+    }
+
+    public string GetColorName(ColorVariants colorType)
+    {
+        if (!m_AvilableColorList.ContainsKey(colorType))
+            return "";
+
+        return m_AvilableColorList[colorType].m_ColorName;
+    }
+
+    public Color GetColor(ColorVariants colorType)
+    {
+        if (!m_AvilableColorList.ContainsKey(colorType))
+            return Color.white;
+
+        return m_AvilableColorList[colorType].m_Color;
+    }
+}
---
+++ CarnivalGameJam/Assets/Scripts/Colors/ColorSystems.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+
+// keeps track of the color UI info and enum type, this is meant for inspector data
+[Serializable]
+public class ColorInfoData
+{
+    public string m_Name; // to display at inspector
+
+    public ColorVariants m_ColorEnumType;
+    public ColorInfo m_ColorInfo;
+}
+
+[Serializable]
+public struct ColorInfo
+{
+    [Header("Color info")]
+    public Color m_Color;
+    [HideInInspector] public string m_ColorName;
+
+    [Header("Color Combo Mixes")]
+    public ColorMixes m_ColorToMix;
+}
+
+[Serializable]
+public struct ColorMixes //since we're only having the primary colors in the game
+{
+    public bool m_Red;
+    public bool m_Blue;
+    public bool m_Yellow;
+}
+
+public enum ColorVariants
+{
+    RED,
+    YELLOW,
+    BLUE,
+    PURPLE, //red + blue
+    ORANGE, //yellow + red
+    GREEN, //blue + yellow
+    BROWN, //red + blue + yellow
+    COLORLESS, //no color
+}
